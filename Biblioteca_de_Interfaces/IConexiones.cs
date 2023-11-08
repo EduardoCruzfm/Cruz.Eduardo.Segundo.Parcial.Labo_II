e714e95b0c0f9fcd954ad7aae86a8873c08e8b5e
Eduardo Cruz <eduardofrankcruzmendez@gmail.com>
2023-11-07 23:34:57 -0300

    Interfaz

--- Biblioteca_de_Interfaces/IConexiones.cs
+++ Biblioteca_de_Interfaces/IConexiones.cs
@@ -1,7 +1,17 @@
-﻿namespace Biblioteca_de_Interfaces
+﻿using Cruz.Eduardo.Primer.Parcial.Labo_II;
+
+namespace Biblioteca_de_Interfaces
 {
     public interface IConexiones
     {
+
+        bool PruebaConexion();
+
+        List<Vehiculo> ObtenerListaDatos();
+
+        bool AgregarDato(Vehiculo v);
+
+        bool ModificarDato(Vehiculo v);
 
     }
 }
