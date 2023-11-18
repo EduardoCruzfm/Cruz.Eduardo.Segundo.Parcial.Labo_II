b0e593fe4dd006ea5b40c5fdca69ceb9f083c32d
Eduardo Cruz <eduardofrankcruzmendez@gmail.com>
2023-11-18 13:40:34 -0300

    Documentacion ala clase DelegadoCambiaEstadoLbl

--- WinFormsAppLoginUser/DelegadoCambiaEstadoLbl.cs
+++ WinFormsAppLoginUser/DelegadoCambiaEstadoLbl.cs
@@ -7,14 +7,29 @@ using System.Threading.Tasks;
 
 namespace WinFormsAppLoginUser
 {
+    /// <summary>
+    /// Delegado para cambiar el estado de un control Label, incluyendo texto y color.
+    /// </summary>
+    /// <param name="mensaje">Mensaje a mostrar en el control Label.</param>
+    /// <param name="color">Color a aplicar al texto del control Label.</param>
     public delegate void CambiaEstadoLbl(string mensaje, Color color);
+
+    /// <summary>
+    /// Clase que gestiona un evento para modificar un control Label con texto y color.
+    /// </summary>
     public class DelegadoCambiaEstadoLbl
     {
-        // Declaración del evento utilizando el delegado 
+        /// <summary>
+        /// Evento para modificar el control Label, utilizando el delegado CambiaEstadoLbl.
+        /// </summary>
         public event CambiaEstadoLbl MoficarLbl;
 
+        /// <summary>
+        /// Método para cambiar el texto y color del control Label utilizando el evento MoficarLbl.
+        /// </summary>
+        /// <param name="mensaje">Mensaje a mostrar en el control Label.</param>
+        /// <param name="color">Color a aplicar al texto del control Label.</param>
         public void CambiaTextoYColorLbl(string marca, Color color) =>
             MoficarLbl?.Invoke(marca, color);
-
     }
 }
